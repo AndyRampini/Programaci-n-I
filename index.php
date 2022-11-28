8c9861eab13776114a813eb518abc9a7bd1cf652
Andrés Rampini <andres.f.rampini@gmail.com>
2022-11-28 15:51:36 -0300

    algo

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Document</title>
 	<link rel="stylesheet" href="style.css">
+	<link  rel="icon"   href="imagenes/ies7-logo-.jpg" type="" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
 	<header>
@@ -14,10 +16,102 @@
 		<a href="login.php" id='login_btn'>Iniciar sesión</a>
 		<a href="register.php" id='signup_btn'>Registrarse</a>
 	</header>
-	<div>
-		<h1>HOLAAAA</h1>
-	</div>
+	<main>
+  <aside>
+    <div id="cuadro6">
+      <H1>¿Quienes somos?</H1>
+    </div>
+  </aside>
+  <section>
+    <div id="cuadro4">
+     <img src="imagenes/ies7-logo-.jpg" alt="UFL">
+    </div>
+    <div id="cuadro5">
+      <H2>Andrés Rampini</H2>
+      <p>Alumno de tercer año de la carrera de Desarrollo de Software.
 
-</body>
+      </p>
+      <P>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil fugiat molestiae dolore id? Eligendi nemo architecto corporis doloremque adipisci, excepturi enim delectus possimus, repudiandae porro alias ad repellendus perspiciatis rem.
+      </P>
+    </div>
+    
+  </section>
+  
+</main>
+ 
+ <footer>
+       
+  <div class="container-footer-all">
+   
+       <div class="container-body">
 
-</html>
+           <div class="colum1">
+               <h1>Mas informacion de la compañia</h1>
+
+               <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus vero consequatur quibusdam soluta exercitationem laudantium, quis corrupti possimus illum, ab impedit reprehenderit voluptates eveniet quas assumenda odio obcaecati rerum neque!</p>
+
+           </div>
+
+           <div class="colum2">
+
+               <h1>Redes Sociales</h1>
+
+               <div class="row1">
+                   <img src="iconos/facebook.png">
+                   <label>Siguenos en Facebook</label>
+               </div>
+               <div class="row1">
+                   <img src="iconos/twitter.png">
+                   <label>Siguenos en Whatsapp</label>
+               </div>
+               <div class="row1">
+                   <img src="iconos/instagram.png">
+                   <label>Siguenos en Instagram</label>
+               </div>
+
+
+
+           </div>
+
+           <div class="colum3">
+
+               <h1>Informacion Contactos</h1>
+
+               <div class="row2">
+                   <img src="iconos/house.png">
+                   <label>Santa Fe,
+                     Venado Tuerto
+                   </label>
+               </div>
+
+               <div class="row2">
+                   <img src="iconos/smartphone.png">
+                   <label>+54 3462518748</label>
+               </div>
+
+               <div class="row2">
+                   <img src="iconos/contact.png">
+                    <label>andres.f.rampini@gmail.com</label>
+               </div>
+
+           </div>
+
+       </div>
+   
+   </div>
+   
+   <div class="container-footer">
+          <div class="footer">
+               <div class="copyright">
+                   © 2022 Todos los Derechos Reservados | <a href="">ANDRES RAMPINI</a>
+               </div>
+
+               <div class="information">
+                   <a href="">Informacion Compañia</a> | <a href="">Privacion y Politica</a> | <a href="">Terminos y Condiciones</a>
+               </div>
+           </div>
+
+       </div>
+</footer>
+	   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
